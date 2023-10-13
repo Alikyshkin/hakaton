3754b3367ab40a0a914ecc80a72173d013eb3549
Кузнецова Алика Анатольевна <a.kuznetsova@nicetu.spb.ru>
2023-10-13 15:10:40 +0300

    feat: initialised project, header, main content

---
+++ client/pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <Header></Header>
+  <div class="bg-gray-50 p-10 rounded-md shadow-md">
+    <h1 class="text-xl font-semibold mb-2">Отделения и банкоматы</h1>
+    <p class="text-gray-600 mb-4">Находите отделения и банкоматы рядом с вами, используя единую карту</p>
+    <div class="relative mb-4">
+      <img src="https://www.vtb.ru/media-files/vtb.ru/shared/images/hero/Gruppa_H_MapATM_1x.webp" alt="3D Map" class="  object-cover rounded-md px-44">
+      <!-- You can replace the pins with SVG or any other icons -->
+      <div class="absolute top-1/4 left-1/3">
+        <span class="bg-blue-500 text-white p-2 rounded-full">
+          <!-- Add your pin icon here -->
+        </span>
+      </div>
+      <div class="absolute top-1/2 left-2/3">
+        <span class="bg-blue-500 text-white p-2 rounded-full">
+          <!-- Add your pin icon here -->
+        </span>
+      </div>
+    </div>
+    <button class="bg-blue-600 text-white p-2 w-full rounded-md hover:bg-blue-700">Смотреть на карте</button>
+  </div>
+
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+
+export default {
+  name: "index.vue",
+  components: {Header},
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/pages/map/ofices.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "ofices"
+}
+</script>
+
+<style scoped>
+
+</style>

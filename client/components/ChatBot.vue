be2e43d7912a946847e7afe6c906de0d0990d529
Кузнецова Алика Анатольевна <a.kuznetsova@nicetu.spb.ru>
2023-10-14 00:20:56 +0300

    fix: few fixes to chatbot

--- client/components/ChatBot.vue
+++ client/components/ChatBot.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="{'collapsed': !chatOpen, 'main-card': true, 'bg-white': true, 'shadow-md': true, 'rounded-lg': true, 'absolute': true }">
-    <div v-if="chatOpen" class="main-title bg-purple-600 text-white p-4 font-bold flex items-center">
+    <div v-if="chatOpen" class="main-title bg-blue-600 text-white p-4 font-bold flex items-center">
       <span>Chatbot</span>
     </div>
     <div v-if="chatOpen" class="chat-area overflow-y-auto px-4 py-6 flex flex-col gap-3">
@@ -9,11 +9,11 @@
     <div v-if="chatOpen" class="input-div flex items-center gap-3 p-4">
       <input class="input-message flex-grow px-4 py-2 border rounded focus:outline-none" name="message" type="text" placeholder="Type your message ..." />
       <button class="input-send focus:outline-none" @click="send">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6 text-purple-600"><path d="M2,21L23,12L2,3V10L17,12L2,14V21Z"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6 text-blue-600"><path d="M2,21L23,12L2,3V10L17,12L2,14V21Z"></path></svg>
       </button>
     </div>
     <button @click="toggleChat"
-            class="chatbot-toggle p-3.5 bg-purple-600 rounded-full absolute right-0 top-0 focus:outline-none hover:bg-purple-700"
+            class="chatbot-toggle p-3.5 bg-blue-600 rounded-full absolute right-0 top-0 focus:outline-none hover:bg-blue-700"
             :class="{ pulse: !chatOpen }">
       <svg v-if="!chatOpen" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6 text-white"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15 4v7H5.17l-.59.59-.58.58V4h11m1-2H3c-.55 0-1 .45-1 1v14l4-4h10c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1zm5 4h-2v9H6v2c0 .55.45 1 1 1h11l4 4V7c0-.55-.45-1-1-1z"/></svg>
       <svg v-if="chatOpen" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6 text-white"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>
@@ -93,17 +93,18 @@ export default {
   position: fixed;
   bottom: 15px;
   left: 15px;
+  border-radius: 50%;
 }
 
 @keyframes pulse {
   0% {
-    box-shadow: 0 0 0 0 rgba(37, 99, 235, 0.6);
+    box-shadow: 0 0 0 0 rgba(30, 144, 255, 0.6); /* DodgerBlue color */
   }
   70% {
-    box-shadow: 0 0 0 10px rgba(37, 99, 235, 0);
+    box-shadow: 0 0 0 10px rgba(30, 144, 255, 0); /* DodgerBlue color */
   }
   100% {
-    box-shadow: 0 0 0 0 rgba(37, 99, 235, 0);
+    box-shadow: 0 0 0 0 rgba(30, 144, 255, 0); /* DodgerBlue color */
   }
 }
 

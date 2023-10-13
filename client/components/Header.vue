efb624e049c883c54c6b29d61ea9a7c7bbdda5f6
Кузнецова Алика Анатольевна <a.kuznetsova@nicetu.spb.ru>
2023-10-13 22:43:06 +0300

    feat: header colors, city selector

--- client/components/Header.vue
+++ client/components/Header.vue
@@ -1,9 +1,14 @@
 <template>
   <nav class="bg-white border-gray-200">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <nuxt-link to="/" class="flex items-center">
-        <img src="https://facultetus.ru/images/logos/c88854675b12151e550165ea0c9d4101.png" class="h-12 mr-3" alt="VTB Logo" />
-      </nuxt-link>
+      <div class="flex items-center">
+        <nuxt-link to="/" class="flex items-center">
+          <img src="https://facultetus.ru/images/logos/c88854675b12151e550165ea0c9d4101.png" class="h-12 mr-3" alt="VTB Logo" />
+        </nuxt-link>
+        <div class="ml-4">
+          <CitySelector></CitySelector>
+        </div>
+      </div>
       <button @click="toggleMenu" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" :aria-expanded="isMenuOpen ? 'true' : 'false'">
         <span class="sr-only">Open main menu</span>
         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -13,19 +18,19 @@
       <div v-bind:class="{ hidden: !isMenuOpen, 'block md:flex': isMenuOpen }" class="w-full md:block md:w-auto" id="navbar-default">
         <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
           <li>
-            <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0" aria-current="page">ВТБ ОНЛАЙН</a>
+            <a href="#" class="block py-2 pl-3 pr-4 rounded md:bg-transparent text-gray-900 md:p-0" aria-current="page">ВТБ ОНЛАЙН</a>
           </li>
           <li>
-            <nuxt-link to="/map/offices" class="block py-2 pl-3 pr-4 text-blue-700 md:p-0">Карта</nuxt-link>
+            <nuxt-link to="/map/offices" class="block py-2 pl-3 pr-4 text-gray-900 md:p-0">Карта</nuxt-link>
           </li>
           <li v-if="!isAuthenticated">
-            <nuxt-link to="/authorization" class="block py-2 pl-3 pr-4 text-blue-700 md:p-0">Войти</nuxt-link>
+            <nuxt-link to="/authorization" class="block py-2 pl-3 pr-4 text-gray-900 md:p-0">Войти</nuxt-link>
           </li>
           <li v-if="!isAuthenticated">
-            <nuxt-link to="/registration" class="block py-2 pl-3 pr-4 text-blue-700 md:p-0">Зарегистрироваться</nuxt-link>
+            <nuxt-link to="/registration" class="block py-2 pl-3 pr-4 text-gray-900 md:p-0">Зарегистрироваться</nuxt-link>
           </li>
           <li v-if="isAuthenticated">
-            <a href="#" @click="logout" class="block py-2 pl-3 pr-4 text-blue-700 md:p-0">Выйти</a>
+            <a href="#" @click="logout" class="block py-2 pl-3 pr-4 text-gray-900 md:p-0">Выйти</a>
           </li>
         </ul>
       </div>
@@ -34,6 +39,8 @@
 </template>
 
 <script>
+import CitySelector from "./CitySelector"
+
 export default {
   name: "Header",
   data() {

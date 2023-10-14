324a0ca5a4c2386ff6b5ed704b7755713b7efd0f
Кузнецова Алика Анатольевна <a.kuznetsova@nicetu.spb.ru>
2023-10-14 13:19:29 +0300

    fix: map sidebar

--- client/components/SideBar.vue
+++ client/components/SideBar.vue
@@ -1,6 +1,6 @@
 <!-- Sidebar.vue -->
 <template>
-  <div class="bg-white h-1/2 w-1/4 p-4 overflow-x-hidden overflow-y-auto ">
+  <div class="bg-white h-1/2 w-1/4 p-4 overflow-x-hidden overflow-y-auto sidebar-map">
     <div v-for="office in offices" :key="office.salePointName" @click="flyTo(office)">
       <h3 class="text-xl font-bold mb-2">{{ office.salePointName }}</h3>
       <p class="mb-1">{{ office.address }}</p>
@@ -19,5 +19,11 @@ export default {
 </script>
 
 <style scoped>
-/* Here, you can add more styles if needed */
+.sidebar-map {
+  position: absolute;
+  top: 90px;
+  left: 15px;
+  height: 80vh;
+  z-index: 1;
+}
 </style>

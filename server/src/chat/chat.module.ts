a3f347a2ecd31d3488e4978a4851d905545a75be
Artyom Babich <artem3845@gmail.com>
2023-10-15 09:08:02 +0300

    feat: added chat support

--- server/src/chat/chat.module.ts
+++ server/src/chat/chat.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
+  imports: [ConfigModule],
   providers: [ChatService],
   controllers: [ChatController]
 })
